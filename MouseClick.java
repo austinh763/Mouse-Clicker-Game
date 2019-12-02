d554211ad192bbaa4d9ad67d32209a9ad73898a5
goldcrumb <58415160+goldcrumb@users.noreply.github.com>
2019-12-01 22:44:52 -0800

    Create MouseClick.java

---
+++ MouseClick.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class MouseClick {
+
+	public static void main(String[] args)
+    {
+        MouseClickClass frame = new MouseClickClass();
+        frame.setVisible(true);       
+    }
+}
+
+
