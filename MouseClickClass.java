68d840f7096bb65cc5aa65ef23594b5d4763a1fd
goldcrumb <58415160+goldcrumb@users.noreply.github.com>
2019-12-02 23:13:50 -0800

    Update MouseClickClass.java

--- MouseClickClass.java
+++ MouseClickClass.java
@@ -1,97 +1,92 @@
-package finalProject;
-
+/*
+ * Austin Hoang
+ * 12/2/2019
+ * Final Project
+ * A mouse clicker game to see how many times a user can hit a random moving X.
+ */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.util.Random;
 
-
-public class MouseClickClass extends JFrame implements ActionListener
-{
+public class MouseClickClass extends JFrame implements ActionListener {
+	//Initializing the correct mark for user to click
     private final String rightClick = "X";
-
-    private final int MAX_ROUNDS = 5;
+    //Initializing number of rounds and number of board buttons.
+    private final int MAX_ROUNDS = 20;
     private int numBoardButtons = 64;
-
+    //Initializing game counters(# clicked, missed, rounds, time delay, setting 
+    //wrongClick to an empty string.
     private int clicked = 0;
     private int misses = 0;
     private int round = 0;
     private int delay = 1500;
+    private int percentage = 0;
     private String wrongClick = " ";
-
-
+    //Initializing board/frame properties
     private JPanel panel = new JPanel();
     private JButton[] boardButtons = new JButton[numBoardButtons];
-    private JLabel instructions;
+    private JLabel gameText;
     private Timer timer;
     private JPanel buttonsPanel = new JPanel();
-    
+    //Initializing buttons
     private JButton start = new JButton("Start");
     private JButton quit = new JButton("Quit");
     private JButton reset = new JButton("Reset");
     private JButton pause = new JButton("Pause");
     private JButton resume = new JButton("Resume");
-    
-    int percentage = 0;
-    boolean caught = false;
-    boolean notCaught = false;
-    boolean gameOver = false;
-
-    
+    //Initializing booleans for caught/notCaught and game over.
+    private boolean caught = false;
+    private boolean notCaught = false;
+    private boolean gameOver = false;
+    //Initializing frame components
     JMenuBar bar = new JMenuBar();
     JMenu difficulty = new JMenu("Change Difficulty");
     JMenuItem easy = new JMenuItem("Easy");
     JMenuItem medium = new JMenuItem("Medium");
     JMenuItem hard = new JMenuItem("Hard");
-    JMenu changeButtons = new JMenu("Change Buttons");
-    JMenuItem noButton = new JMenuItem("No button");
+    JMenu changeButtons = new JMenu("Change Board Marks");
+    JMenuItem noButton = new JMenuItem("No marks");
     JMenuItem button1 = new JMenuItem("0");
     JMenuItem button2 = new JMenuItem("8");
 
-
-    /**
-     * Constructor.
-     **/
-    public MouseClickClass()
-    {
-        // Setup the JFrame...
+    public MouseClickClass() {
+        // Setting up the JFrame.
         super("Mouse Clicker");
         setSize(1280, 720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
 
-        // Game instructions and components.
-        instructions = new JLabel("");
+        //Setting up game instructions with font and game panel.
+        gameText = new JLabel("");
         Font myFont = new Font("Courier New", Font.BOLD, 16);
         JPanel game = new JPanel();
         
 
-        // Padding around edges.
+        //Setting layouts
         game.setLayout(new GridLayout(8, 8));
         panel.setLayout(new BorderLayout());
         panel.setBorder(new EmptyBorder(15, 15, 15, 15));
         setJMenuBar(bar);
 
-        // Create each button.
-        for (int i = 0; i < numBoardButtons; ++i)
-        {
+        //Creating each button of the grid layout.
+        for (int i = 0; i < numBoardButtons; ++i) {
             boardButtons[i] = new JButton(wrongClick);
             boardButtons[i].setFont(myFont);
             boardButtons[i].addActionListener(this);
             boardButtons[i].setOpaque(true);
             boardButtons[i].setBorderPainted(false);
             boardButtons[i].setBackground(Color.WHITE);
-            boardButtons[i].setForeground(Color.BLACK);
             boardButtons[i].setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
             game.add(boardButtons[i]);
         }
 
-        // Setup timer.
+        //Setting up the timer.
         timer = new Timer(delay, this);
         timer.setInitialDelay(delay);
         timer.stop();
-        
+        //Adding action listeners to all buttons
         start.addActionListener(this);
         quit.addActionListener(this);
         reset.addActionListener(this);
@@ -104,9 +99,8 @@ public class MouseClickClass extends JFrame implements ActionListener
         button1.addActionListener(this);
         button2.addActionListener(this);
 
-
-        // Add elements.
-        panel.add(instructions, BorderLayout.NORTH);
+        //Adding elements elements.
+        panel.add(gameText, BorderLayout.NORTH);
         panel.add(game, BorderLayout.CENTER);
         buttonsPanel.add(start);
         buttonsPanel.add(quit);
@@ -123,32 +117,23 @@ public class MouseClickClass extends JFrame implements ActionListener
         changeButtons.add(noButton);
         changeButtons.add(button1);
         changeButtons.add(button2);
-        //moveMouse();
         updateInstructions();     
         resume.setVisible(false);
     }
 
-    /**
-     * Returns a random index to be used with the boardButtons array.
-     */
-    public int getRandomPlacement()
-    {
+    //Returns a random index where the X will move
+    public int getRandomPlacement() {
         Random random = new Random();
         return random.nextInt(numBoardButtons);
     }
         	
-    /**
-     * Using the random index, determine which button will be the "mouse".
-     */
-    public void moveMouse()
-    {    	
+    //Using random index to move X
+    public void moveMark() {    	
         timer.restart();
         int index = getRandomPlacement();
-        for (int i = 0; i < numBoardButtons; ++i)
-        {
+        for (int i = 0; i < numBoardButtons; ++i) {
             boardButtons[i].setText(wrongClick);
-            if (i == index)
-            {
+            if (i == index) {
                 boardButtons[i].setText(rightClick);
                 boardButtons[i].setBackground(Color.WHITE);
                 boardButtons[i].setForeground(Color.BLACK);
@@ -156,107 +141,102 @@ public class MouseClickClass extends JFrame implements ActionListener
         }
     }
         
-    /**
-     * Resets the core variables so the player can play again.
-     */
-    private void resetGame()
-    {
+    //Resetting game variables
+    private void resetGame() {
         clicked = 0;
         misses = 0;
         round = 0;
         gameOver = false;
-        moveMouse();
+        moveMark();
     }
 
-    /**
-     * Prints the current round to the user.
-     */
-    private void updateInstructions()
-    {
+    //Instructions updated to show current round, misses, and delay
+    private void updateInstructions() {
         String roundStr = "<html>Click the ''X''<br/>Round: " + round + " of " + MAX_ROUNDS + "<br/>Misses:<html/> " 
         		+ misses + "<br>Delay: " + delay + "ms";
-        instructions.setText(roundStr);
+        gameText.setText(roundStr);
+        //If the rounds reach the limit, set gameOver to true
         if(round >= MAX_ROUNDS-1) {
         	gameOver = true;
         }
     }
 
-    /**
-     * Event handling.
-     */
+
     @Override
     public void actionPerformed(ActionEvent e) {
     Object source = e.getSource();
     
-        // The timer finishes. Move the mouse!
-        if (source == timer)
-        {
-            moveMouse();
+        //The timer finishes, move the X.
+        if (source == timer) {
+            moveMark();
             ++misses;
             round++;
             updateInstructions();
         }
+        //Starts the game.
         else if(source == start) {
         	timer.start();
-        	moveMouse();
+        	moveMark();
         }
+        //Calls resetGame() and resets the game.
         else if(source == reset) {
         	resetGame();
         	updateInstructions();
         	timer.stop();
         }
+        //Closes the window
         else if(source == quit) {
         	super.dispose();
         }
+        //Pauses the game by stopping the timer
         else if(source == pause) {
         	timer.stop();
+        	//Setting resume visibility to true 
         	resume.setVisible(true);     	        	
         }
+        //Restarts the game from a pause
         else if(source == resume) {
         	timer.start();
         	resume.setVisible(false);
         }
+        //Sets the time delay to 1500 (easy)
         else if(source == easy) {
         	timer.setInitialDelay(1500);
         	delay = 1500;
 
         }
+        //Sets the time delay to 1100 (medium)
         else if(source == medium) {
-        	timer.setInitialDelay(1300);
-        	delay = 1300;
-
+        	timer.setInitialDelay(1100);
+        	delay = 1100;
         }
+        //Sets the time delay to 800 (hard)
         else if(source == hard) {
-        	timer.setInitialDelay(700);
-        	delay = 700;
-
+        	timer.setInitialDelay(800);
+        	delay = 800;
         }
+        //Sets the game board buttons to empty (no marks, just X)
         else if(source == noButton) {
         	wrongClick = " ";
         }
+        //Sets game board buttons to 0
         else if(source == button1) {
         	wrongClick = "0";
         }
+        //Sets game board buttons to 8
         else if(source == button2) {
         	wrongClick = "8";
         }
 
-        // A button was clicked...
-        else 
-        {
+        //Else if a button was clicked on the board.
+        else {
             JButton button = (JButton)e.getSource();
-            
-            int percentage = 0;
-            boolean caught = false;
-            boolean notCaught = false;
-			
-            // ...the user clicked on the mouse!
-            if (button.getText().equals(rightClick))
-            {
+            			
+            //User clicked on the X
+            if (button.getText().equals(rightClick)) {
                 ++clicked;
                 round++;
-                for (int i = 0; i < numBoardButtons; ++i)
-                {
+                for (int i = 0; i < numBoardButtons; ++i) {
                     boardButtons[i].setBackground(Color.WHITE);
                     boardButtons[i].setForeground(Color.BLACK);
                 }
@@ -264,37 +244,32 @@ public class MouseClickClass extends JFrame implements ActionListener
                 notCaught = false;
             }
 
-            // ...the user clicked on something else. Add a miss to their score.
-            else if(button.getText().equals(wrongClick))
-            {	
+            //User clicked something else on the board
+            else if(button.getText().equals(wrongClick)) {	
             	round++;
             	++misses; 
                 notCaught = true;
             }            
-            // The game is finished. Display the score to the user and reset the game.
-        	if (gameOver == true)
-            {	timer.stop();
+            //Game is finished, displays score and misses.
+        	if (gameOver == true) {	
+        		timer.stop();
                 percentage = (int) Math.round(clicked / (double) (misses + clicked) * 100);
-                JOptionPane.showMessageDialog(null, "Finished! \nYou caught the mouse " + percentage + "% of the time!\nMisses: " + misses + "\n");
+                JOptionPane.showMessageDialog(null, "Finished! \nYou correctly clicked " + percentage + 
+                		"% of the time!\nMisses: " + misses + "\n");
             }
-
-
-            // The game is not finished. Move the mouse.
-            
-            else if (caught == true)
-            {            	
-                //button.setBackground(Color.GREEN);
-                //button.setForeground(Color.WHITE);
+        	//Else the game is not finished.
+            else if (caught == true) {            	
+                button.setBackground(Color.GREEN);
+                button.setForeground(Color.WHITE);
                 updateInstructions();
-                moveMouse();
+                moveMark();
             }
             else if(notCaught == true) {           	
             	button.setBackground(Color.RED);
                 button.setForeground(Color.WHITE);
                 updateInstructions();
-                moveMouse();
-            }
-                        
+                moveMark();
+            }                        
         }
     }
 }
